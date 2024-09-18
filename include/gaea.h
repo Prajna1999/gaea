e738014bf7b4d270f937a426ff39359792987fed
Prajna1999 <gituprajna20@gmail.com>
2024-09-18 23:40:05 +0530

    ADD: type header files for basic primitives

--- include/gaea.h
+++ include/gaea.h
@@ -3,4 +3,37 @@
 
 // Gaea database function declarations will go here
 
+#include <stdint.h>
+
+typedef enum
+{
+    GAEA_NULL,
+    GAEA_BOOLEAN,
+    GAEA_INTEGER,
+    GAEA_DOUBLE,
+    GAEA_STRING,
+} GaeaValueType;
+
+typedef struct
+{
+    GaeaValueType type;
+    union
+    {
+        int boolean;
+        int64_t integer;
+        double floating;
+        char *string;
+    } value
+} GaeaValue;
+
+// functions to create different types of GaeaValues:
+GaeaValue gaea_create_null();
+GaeaValue gaea_craete_boolean(int value);
+GaeaValue gaea_craete_integer(int64_t value);
+GaeaValue gaea_create_double(double value);
+GaeaValue gaea_craete_string(const char *value);
+
+// Function to free any resources associated with a GaeaValue:
+void gaea_free_value(GaeaValue *value);
+
 #endif // GAEA_H
